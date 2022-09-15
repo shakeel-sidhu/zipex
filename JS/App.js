920ca8ed2af562c8accc99e18760476aef04e78b
Shakeel Sidhu <shakeeljutt070@gmail.com>
2022-09-15 23:39:58 +0500

    first version release

---
+++ JS/App.js
@@ -0,0 +1,151 @@
+
+  /*--------- Table All Contents --------*/
+
+$('documet').ready(function(){
+
+    /*---------Make Money Link--------*/
+
+    $('#makeMoney-Link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".make-money").offset().top
+        }, 1000);
+    })
+   /*---------Make Mehtod 1--------*/
+
+    $('.Method1-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-1").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 2--------*/
+
+    $('.Method2-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-2").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 3--------*/
+
+    $('.Method3-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-3").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 4--------*/
+
+    $('.Method4-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-4").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 5--------*/
+
+    $('.Method5-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-5").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 6--------*/
+
+    $('.Method6-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-6").offset().top
+        }, 1000);
+    })
+
+    /*---------Make Mehtod 7--------*/
+
+    $('.Method7-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".method-7").offset().top
+        }, 1000);
+    })
+
+    /*--------- Investing Worth --------*/
+
+    $('.investingWorth-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".investingWorth").offset().top
+        }, 1000);
+    })
+
+    /*--------- Make-Money --------*/
+
+    $('.makeMoney-Btc-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".makeMoney-Btc").offset().top
+        }, 1000);
+    })
+
+    /*--------- Final-Thoughts --------*/
+    $('.final-thoughts-link').click(function(){
+        // console.log("Hello")
+        $('html, body').animate({
+         scrollTop: $(".final-thoughts").offset().top
+        }, 1000);
+    })
+
+})
+
+
+
+
+
+
+
+const array = [1,2,3,4,5]
+
+const b = array.map((test)=>{
+    return test[1] * 3
+})
+console.log(b)
+// function test(index) {
+
+//     return index * 5;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
